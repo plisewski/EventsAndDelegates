435225e32b4083dbb1756fd3f0b6e8ef34e879ec
Piotr Lisewski <p.lisewski@hotmail.com>
2018-03-16 20:46:29 +0100

    Add project files.

---
+++ EventsAndDelegates/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class Book
+    {
+        private string _name;
+
+        public event NameChangedDelegate NameChanged;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                // as _name change occurs here, this the place where the event is invoked
+
+                if (!String.IsNullOrEmpty(value))
+                {
+                    if (_name != value && NameChanged != null)
+                    {
+                        NameChangedEventArgs args = new NameChangedEventArgs();
+                        args.ExistingName = _name;
+                        args.NewName = value;
+
+                        NameChanged(this, args);
+                    }
+                    _name = value;
+                }
+            }
+        }
+    }
+}
---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book() { Name = "Empty" };
+
+            book.NameChanged += new NameChangedDelegate(OnNameChanged);
+
+            book.Name = "New Name";
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Book changing name from {args.ExistingName} to {args.NewName}");            
+        }
+    }
+}
